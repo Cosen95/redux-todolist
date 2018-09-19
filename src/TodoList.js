5b099d0526a9d13a5fe9a7d15d600c777cc8ca75
fengshuan <1263215592@qq.com>
2018-09-19 23:23:14 +0800

    feat:store和reducer创建

--- src/TodoList.js
+++ src/TodoList.js
@@ -1,25 +1,44 @@
 import React,{ Component } from 'react';
 import 'antd/dist/antd.css';
 import { Input, Button, List } from 'antd';
-
-const data = [
-    'Racing car sprays burning fuel into crowd.',
-    'Japanese princess to wed commoner.',
-    'Australian walks 100km after outback crash.',
-    'Man charged over missing wedding girl.',
-    'Los Angeles battles huge wildfires.',
-  ];
+import store from './store';
 
 class TodoList extends Component{
+    constructor(props) {
+        super(props);
+        this.state = store.getState();
+        store.subscribe(this.handleStoreChange);
+        // console.log(store.getState());
+
+    }
+    handleInputChange = (e)=> {
+        store.dispatch({
+            type: 'change_input_value',
+            value: e.target.value
+        })
+    }
+    handleStoreChange = ()=> {
+        this.setState(store.getState());
+    }
+    handleSubmit = ()=> {
+        store.dispatch({
+            type: 'add_todo_item'
+        })
+    }
     render(){
         return(<div style={{ marginTop:20, marginLeft:20 }}>
             <div style={{ marginBottom:20 }}>
-                <Input placeholder="请输入" style={{ width:300, marginRight:10}} />
-                <Button type="primary">提交</Button>
+                <Input
+                 defaultValue={this.state.inputValue}
+                 placeholder="请输入" 
+                 style={{ width:300, marginRight:10}}
+                 onChange={this.handleInputChange}
+                />
+                <Button type="primary" onClick={this.handleSubmit}>提交</Button>
             </div>
             <List
                 style={{ width:500 }}
-                dataSource={data}
+                dataSource={this.state.list}
                 renderItem={item => (<List.Item>{item}</List.Item>)}
             />
         </div>)
---
+++ src/store/reducer.js
@@ -0,0 +1,19 @@
+const defaultState = {
+    inputValue: 'redux',
+    list: ['saga','dva']
+}
+
+export default (state = defaultState, action) => {
+    if (action.type === 'change_input_value') {
+        const newState = JSON.parse(JSON.stringify(state));
+        newState.inputValue = action.value;
+        return newState;
+    }
+    if (action.type === 'add_todo_item') {
+        const newState = JSON.parse(JSON.stringify(state));
+        newState.list.push(newState.inputValue);
+        newState.inputValue = '';
+        return newState;
+    }
+    return state;
+}
